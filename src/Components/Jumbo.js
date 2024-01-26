3c85768048907fd6091c0a44ed9c3f68b5d44af1
Brandon Ruiz <Sober4202020@outlook.com>
2024-01-26 00:48:17 +0000

    adding

---
+++ src/Components/Jumbo.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import style from './css/jumbo-style.module.css';
+
+function Jumbo() {
+	return (
+		<>
+		<div className={style["hero-image"]}>
+		<div className={style["hero-text"]}>
+		<h1>Affordable Landscape & Maintenance LLC</h1>
+		</div>
+		</div>
+		</>
+
+	);
+}
+
+export default Jumbo;
--- src/Components/Topnav.js
+++ src/Components/Topnav.js
@@ -4,15 +4,15 @@ import Nav from 'react-bootstrap/Nav';
 function Topnav() {
 	  return (
 		      <>
-		        <Nav className="justify-content-evenly" style={{alignItems: "center", justifyContent: "center", height: "150px",backgroundColor: "grey"}} bg="dark" activeKey="/home">
-		          <Nav.Item>
-		            <Nav.Link href="/home">About</Nav.Link>
+		        <Nav className="justify-content-evenly" style={{alignItems: "center", justifyContent: "center", height: "60px", backgroundColor: "grey"}}  >
+		  <Nav.Item>
+		            <Nav.Link href="/home" style={{color: "white"}}>About</Nav.Link>
 		          </Nav.Item>
 		          <Nav.Item>
-		            <Nav.Link eventKey="link-1">Services</Nav.Link>
+		            <Nav.Link style={{color: "white"}} >Services</Nav.Link>
 		          </Nav.Item>
 		          <Nav.Item>
-		            <Nav.Link eventKey="link-2">Contact</Nav.Link>
+		            <Nav.Link  style={{color: "white"}} >Contact</Nav.Link>
 		          </Nav.Item>
 		        </Nav>
 		  </>
