32465c1007cd238e370ac5b2bd53a393c38ea791
Brandon Ruiz <Sober4202020@outlook.com>
2024-01-30 03:57:08 +0000

    fixing bottom nav bar

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import Container from 'react-bootstrap/Container';
+import Navbar from 'react-bootstrap/Navbar';
 import Nav from 'react-bootstrap/Nav';
 import { SiNextdoor } from 'react-icons/si';
 import { FaFacebook } from 'react-icons/fa';
@@ -6,24 +8,30 @@ import { FaInstagram } from 'react-icons/fa6';
 import ConfigIcon from './ConfigIcon';
 import ConfigIconNext from './ConfigIconNext';
 
-
-
 function Footer() {
 	  return (
 		      <>
-		        <Nav className="justify-content-evenly" style={{alignItems: "center", justifyContent: "center", height: "90px", borderTop: "solid", borderColor: "rgba(128,128,0,0.6)", backgroundColor: "rgba(128,128,0,0.6)"}}  >
-		  <Nav.Item>
-		            <Nav.Link href="/home"><ConfigIcon><FaInstagram/></ConfigIcon></Nav.Link>
-		          </Nav.Item>
-		          <Nav.Item>
-		            <Nav.Link><ConfigIcon><FaFacebook/></ConfigIcon></Nav.Link>
-		          </Nav.Item>
-		          <Nav.Item>
-		            <Nav.Link href="https://nextdoor.com/profile/01yb--h-kMGCpCKHF/?init_source=search&is=search&query=Herbert%20Nashion"><ConfigIconNext><SiNextdoor/></ConfigIconNext></Nav.Link>
-		          </Nav.Item>
-		        </Nav>
-		  </>
-	  );
+		        <Container>
+		          <Navbar>
+		            <Container className="justify-content-center">
+		              <Nav>
+		                <Nav.Link href="https://nextdoor.com/profile/01yb--h-kMGCpCKHF/?is=feed_author">
+		                  <SiNextdoor />
+		                </Nav.Link>
+		                <Nav.Link href="https://nextdoor.com/profile/01yb--h-kMGCpCKHF/?is=feed_author">
+		                  <FaFacebook />
+		                </Nav.Link>
+		                <Nav.Link href="https://nextdoor.com/profile/01yb--h-kMGCpCKHF/?is=feed_author">
+		                  <FaInstagram />
+		                </Nav.Link>
+		                {/* You can add more icons with corresponding Nav.Link */}
+		              </Nav>
+		            </Container>
+		          </Navbar>
+		        </Container>
+		      </>
+		    );
 }
 
 export default Footer;
+
