32ee07074718df9cbad1c6e79bd528607a739cd0
Brandon Ruiz <Sober4202020@outlook.com>
2025-05-12 00:11:29 +0000

    made edit

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -19,9 +19,6 @@ function Footer() {
 		                <Nav.Link href="https://www.facebook.com/profile.php?id=61553296473902">
 		                  <FaFacebook />
 		                </Nav.Link>
-		                <Nav.Link href="https://nextdoor.com/profile/01yb--h-kMGCpCKHF/?is=feed_author">
-		                  <FaInstagram />
-		                </Nav.Link>
 		                {/* You can add more icons with corresponding Nav.Link */}
 		              </Nav>
 		            </Container>
